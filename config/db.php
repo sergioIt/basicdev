5ec5393a4109ba626f6e65de3bbb3cc25574e807
Sergio <serge.kite@gmail.com>
2015-12-10 11:17:59 +0300

    remove db config from git

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=basic.dev',
-    'username' => 'root',
-    'password' => 'root',
-    'charset' => 'utf8',
-];
